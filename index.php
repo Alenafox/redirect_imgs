d0cd4470cd1e89c9a375fef93648906a2f35f9d9
Alenafox <57696470+Alenafox@users.noreply.github.com>
2020-11-13 12:55:52 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$host = 'localhost'; 
+$database = 'formdb12'; 
+$user = 'formdb12'; 
+$password = '729YMxrY'; 
+
+$link = mysqli_connect($host, $user, $password, $database); 
+
+if (!$link) {
+  echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
+  exit;
+}
+use function call_user_func as f;
+use UnexpectedValueException as e;
+
+$image_name = $_GET['image_name'];
+
+$query = "SELECT url FROM images WHERE name='$image_name'";
+$res = mysqli_query($link, $query) or die(mysqli_error($link));
+var_dump($res);
+$row = mysqli_fetch_row($res);
+
+header('Location: '.$row[0]);
+?>
